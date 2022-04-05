85bc57eb94ae120d155fbdf760c6112be712ba33
Isaiah Narisma <narisma.isaiah@gmail.com>
2022-04-05 07:42:22 -0700

    Addition of mediterranean-dream practice OOP

---
+++ python-oop-practice/mediterranean-dream/mediterranean-dream-oop.py
@@ -0,0 +1,46 @@
+# Mediterranean Dream OOP practice project
+# Overview of room options
+# basic double room,
+# suite,
+# single room,
+# shared room in girls only and boys only rooms
+
+# Other services
+# breakfast service
+# sauna service
+# pool service
+# spa service
+
+# Customers can book as many services as they wish
+
+# 1. create room class
+
+class Room:
+    def __init__(self, price, size):
+        self.price = price
+        self.size = size
+
+    def bookroom(self):
+        print("You can book this " + self.size + " room for $" + str(self.price) + " per night.")
+
+
+studio = Room(60, "studio")
+studio.bookroom()
+
+
+# 2. exploit inheritance
+
+class Suite(Room):
+    def booksuite(self):
+        print("You can book this " + self.size + " room for $" + str(self.price) + " per night.")
+    # should not have to make this method again? inherited from the Room class
+
+
+suite1 = Suite(90, "Suite")
+suite1.booksuite()
+
+
+class Shared(Room):
+    pass
+
+# 3. inherit from class Shared, with more attributes
